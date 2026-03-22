02d78b606989ba537e7dab2c2233f394f8cd0e77
Athaya Razan <142677331+Numenor08@users.noreply.github.com>
2026-03-22 10:24:55 +0700

    fix: automization uploads folder creation

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import uploadRoute from '@/routes/upload.route.js'
 import colors from 'colors'
 import streamRoute from './routes/stream.route.js'
 import cors from 'cors'
+import fs from 'fs'
 
 const PORT = process.env.PORT || 3000
 const server = express()
+if (!fs.existsSync('uploads')) {
+    fs.mkdirSync('uploads')
+}
+
 server.use(cors())
 server.use('/api', uploadRoute)
 server.use('/api', streamRoute)
